a4e55cd54864f98003cece70bc3ee3d08086600d
zhtzoe <690098031@qq.com>
2019-02-25 20:05:50 +0800

    fix #5 add database tables for user, job and company

--- jobplus/app.py
+++ jobplus/app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_migrate import Migrate
+
 from .config import configs
 from .handlers import front
+from jobplus.models import db
+
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(configs.get(config))
+    db.init_app(app)
+    Migrate(app,db)
     app.register_blueprint(front)
 
     return app
