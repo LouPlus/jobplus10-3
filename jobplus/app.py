8b7eab69fd08927d908ea884cc83afdf72c470f2
zhtzoe <690098031@qq.com>
2019-02-28 12:17:25 +0800

    temp commit for #16

--- jobplus/app.py
+++ jobplus/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 from .config import configs
 from .handlers import front
-from jobplus.models import db
+from jobplus.models import db, User
 
 
 def register_blueprints(app):
@@ -14,9 +15,21 @@ def register_blueprints(app):
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(configs.get(config))
-    # db.init_app(app)
-    # Migrate(app, db)
+    register_extensions(app)
     register_blueprints(app)
 
     return app
+
+def register_extensions(app):
+    db.init_app(app)
+    Migrate(app, db)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def user_loader(id):
+        return User.query.get(id)
+
+    login_manager.login_view = 'front.login'
 
--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import login_user
+
+from jobplus.forms import LoginForm
+from jobplus.models import User
 
 front = Blueprint('front', __name__)
 
@@ -7,3 +11,11 @@ front = Blueprint('front', __name__)
 def index():
     return render_template('index.html')
 
+@front.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        login_user(user, form.remember_me.data)
+        return redirect(url_for('.index'))
+    return render_template('login.html',form=form)
